24bdb1f97f8d940efbdc1bcf8c6102e09d8439cc
Julien Colombain <julien@colombain.com>
2020-11-14 15:17:41 +0100

    :sparkles: updated linter config

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	printWidth: 110,
+	tabWidth: 2,
+	useTabs: false,
+	semi: true,
+	singleQuote: true,
+	quoteProps: 'as-needed',
+	bracketSpacing: true,
+	trailingComma: 'es5',
+	arrowParens: 'always',
+	jsxSingleQuote: false,
+	jsxBracketSameLine: false,
+};
